6525f23e28587a138cfc4c88238d9c4c1d1b55c4
somnusyyy <47393005+somnusyyy@users.noreply.github.com>
2021-01-11 21:11:08 +0800

    Update update.py

--- update.py
+++ update.py
@@ -5,8 +5,26 @@ import xml.etree.ElementTree as ET
 feed = requests.get('https://somnusyyy.github.io/atom.xml').text
 root = ET.fromstring(feed)
 
+with open('README.md', 'w') as f:
+    f.write(r'''
+    ```
+ _          _ _                            _     _ 
+| |__   ___| | | ___   __      _____  _ __| | __| |
+| '_ \ / _ | | |/ _ \  \ \ /\ / / _ \| '__| |/ _` |
+| | | |  __| | | (_) |  \ V  V | (_) | |  | | (_| |
+|_| |_|\___|_|_|\___/    \_/\_/ \___/|_|  |_|\__,_|
+                                                   
+
+* 1999
+* Skills: C/Java/Python
+* Interests: OS, Coding
+```
+''')
+    
+f.write('''
+
 
 ## Statistics
-![Stats](https://github-readme-stats.vercel.app/api?username=netcan)
-![Lang](https://github-readme-stats.vercel.app/api/top-langs/?username=netcan&hide=ipynb,html&layout=compact)
+![Stats](https://github-readme-stats.vercel.app/api?username=somnusyyy)
+![Lang](https://github-readme-stats.vercel.app/api/top-langs/?username=somnusyyy&hide=ipynb,html&layout=compact)
 ''')

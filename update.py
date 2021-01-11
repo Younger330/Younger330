4e9e70815c01c6966f77de476cb085838f6e07d2
somnusyyy <47393005+somnusyyy@users.noreply.github.com>
2021-01-11 18:17:22 +0800

    Update update.py

--- update.py
+++ update.py
@@ -2,6 +2,9 @@ import re
 import requests
 import xml.etree.ElementTree as ET
 
+feed = requests.get('https://somnusyyy.github.io/atom.xml').text
+root = ET.fromstring(feed)
+
 
 ## Statistics
 ![Stats](https://github-readme-stats.vercel.app/api?username=netcan)

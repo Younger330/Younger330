165a9de3f4e324ee171b5952e619ea3848aeee5d
somnusyyy <47393005+somnusyyy@users.noreply.github.com>
2021-01-11 18:11:23 +0800

    Create update.py

---
+++ update.py
@@ -0,0 +1,9 @@
+import re
+import requests
+import xml.etree.ElementTree as ET
+
+
+## Statistics
+![Stats](https://github-readme-stats.vercel.app/api?username=netcan)
+![Lang](https://github-readme-stats.vercel.app/api/top-langs/?username=netcan&hide=ipynb,html&layout=compact)
+''')
